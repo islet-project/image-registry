26a0658a2081c763491a386f4dce647a3aaa3d9b
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2024-04-04 13:32:58 +0200

    Add fallback handler, distinguish between manifests and images

--- server/src/httpd.rs
+++ server/src/httpd.rs
@@ -1,7 +1,7 @@
 use crate::{registry::Registry, Config};
 use axum::{
     extract, http,
-    response::{self, IntoResponse},
+    response::IntoResponse,
     routing, Json, Router,
 };
 use std::sync::Arc;
@@ -13,14 +13,18 @@ use uuid::Uuid;
 
 type SafeReg = Arc<RwLock<Registry>>;
 
+static NOT_FOUND: (http::StatusCode, &'static str) =
+    (http::StatusCode::NOT_FOUND, "In the beginning there was darkness");
+
 pub async fn run(reg: Registry)
 {
     let reg = Arc::new(RwLock::new(reg));
 
-    let http_json = format!("/{}/:id", Config::readu().http);
+    let http_json = format!("/{}/*file", Config::readu().http);
     let app = Router::new()
         .route(&http_json, routing::get(http_get))
         .with_state(reg)
+        .fallback(fallback)
         .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
 
     let address = format!("0.0.0.0:{}", Config::readu().port);
@@ -28,15 +32,33 @@ pub async fn run(reg: Registry)
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn http_get(extract::Path(id): extract::Path<String>,
-                  extract::State(reg): extract::State<SafeReg>)
-                  -> impl response::IntoResponse
+async fn fallback() -> (http::StatusCode, &'static str)
 {
+    NOT_FOUND
+}
+
+async fn http_get(extract::Path(file): extract::Path<String>,
+                  extract::State(reg): extract::State<SafeReg>)
+                  -> impl IntoResponse
+{
+    let v: Vec<&str> = file.split('.').collect();
+    if v.len() != 2 {
+        return NOT_FOUND.into_response();
+    }
+
     let reg = reg.read().await;
-    let uuid = Uuid::parse_str(&id).unwrap_or(Uuid::default());
-    if !reg.contains_key(&uuid) {
-        (http::StatusCode::NOT_FOUND, "Requested UUID not found").into_response()
-    } else {
-        Json(&reg[&uuid].manifest).into_response()
+    match v[1].to_lowercase().as_str() {
+        "json" => {
+            let uuid = Uuid::parse_str(&v[0]).unwrap_or(Uuid::default());
+            if !reg.contains_key(&uuid) {
+                (http::StatusCode::NOT_FOUND, "Manifest not found").into_response()
+            } else {
+                Json(&reg[&uuid].manifest).into_response()
+            }
+        },
+        "tgz" | "tar.gz" | "tbz" | "tar.bz2" | "zip" => {
+            (http::StatusCode::NOT_FOUND, "Image not found").into_response()
+        }
+        _ => NOT_FOUND.into_response()
     }
 }
