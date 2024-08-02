e995a9fddb02743ed99c6ef5b75c358699338ee8
Lukasz Pawelczyk <l.pawelczyk@samsung.com>
2024-08-02 17:57:23 +0200

    Simple range implementation
    For now only a simple "range: bytes=SKIP-" is supported.

--- ir-server/src/httpd.rs
+++ ir-server/src/httpd.rs
@@ -1,16 +1,17 @@
-use axum::http::HeaderName;
-use axum::{body, extract, http, response::IntoResponse, routing, Json, Router};
-use log::{debug, info};
+use axum::{body::Body, extract, http, response::IntoResponse, routing, Json, Router};
+use axum_extra::{headers::Range, TypedHeader};
+use hyper::Response;
+use log::{debug, error, info};
 use serde::Deserialize;
 use serde_json::json;
-use std::sync::Arc;
-use tokio::sync::RwLock;
+use std::{ops::Bound, sync::Arc};
+use tokio::{io::AsyncSeekExt, sync::RwLock};
 use tokio_util::io::ReaderStream;
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
 
 use crate::config::{Config, Protocol};
-use crate::registry::ImageRegistry;
+use crate::registry::{ImageRegistry, Payload};
 use crate::tls;
 use crate::RegistryResult;
 
@@ -19,7 +20,7 @@ static NOT_FOUND: (http::StatusCode, &str) = (
     "In the beginning there was darkness... Or was it 404? I can't remember.",
 );
 
-const HEADER_DIGEST: HeaderName = HeaderName::from_static("docker-content-digest");
+const HEADER_DIGEST: http::HeaderName = http::HeaderName::from_static("docker-content-digest");
 
 type SafeReg = Arc<RwLock<dyn ImageRegistry>>;
 
@@ -61,6 +62,65 @@ async fn fallback() -> (http::StatusCode, &'static str)
     NOT_FOUND
 }
 
+fn range_not_acceptable(payload: Payload) -> Response<Body>
+{
+    let headers = [(
+        http::header::CONTENT_RANGE,
+        &format!("bytes */{}", payload.size),
+    )];
+    let body = Body::empty();
+    (http::StatusCode::NOT_ACCEPTABLE, headers, body).into_response()
+}
+
+async fn serve_file(mut payload: Payload, range: Option<TypedHeader<Range>>) -> Response<Body>
+{
+    let Some(TypedHeader(range)) = range else {
+        let headers = [
+            (http::header::CONTENT_TYPE, &payload.media_type),
+            (http::header::CONTENT_LENGTH, &format!("{}", payload.size)),
+            (HEADER_DIGEST, &payload.digest),
+        ];
+        let body = Body::from_stream(ReaderStream::new(payload.file));
+        return (headers, body).into_response();
+    };
+
+    let ranges: Vec<_> = range.satisfiable_ranges(payload.size).collect();
+    if ranges.is_empty() {
+        error!("Ranges are not satisfiable");
+        return range_not_acceptable(payload);
+    }
+
+    match (ranges.len(), ranges[0]) {
+        // very basic implementation of "range: bytes=SKIP-" client header
+        (1, (Bound::Included(skip), Bound::Unbounded)) => {
+            if let Err(e) = payload.file.seek(std::io::SeekFrom::Start(skip)).await {
+                error!("Error seeking file: {}", e);
+                return range_not_acceptable(payload);
+            };
+
+            let body = Body::from_stream(ReaderStream::new(payload.file));
+            let headers = [
+                (http::header::CONTENT_TYPE, &payload.media_type),
+                (
+                    http::header::CONTENT_LENGTH,
+                    &format!("{}", payload.size - skip),
+                ),
+                (HEADER_DIGEST, &payload.digest),
+                (
+                    http::header::CONTENT_RANGE,
+                    &format!("bytes {}-{}/{}", skip, payload.size - 1, payload.size),
+                ),
+            ];
+            (http::StatusCode::PARTIAL_CONTENT, headers, body).into_response()
+        }
+        // reject all the other variants of partial content or multi-parts
+        _ => {
+            error!("Only a simple \"range: bytes=SKIP-\" is supported");
+            range_not_acceptable(payload)
+        }
+    }
+}
+
 async fn get_support() -> impl IntoResponse
 {
     (http::StatusCode::OK, "OCI Distribution Spec V2 supported").into_response()
@@ -86,13 +146,11 @@ async fn get_tags(
         return NOT_FOUND.into_response();
     };
 
-    tags.sort_by(|a, b| {
-        a.to_lowercase().partial_cmp(&b.to_lowercase()).unwrap()
-    });
+    tags.sort_by(|a, b| a.to_lowercase().partial_cmp(&b.to_lowercase()).unwrap());
 
     if let Some(last) = params.last {
         tags = if let Some(pos) = tags.iter().position(|x| x == &last) {
-            tags.split_off(pos+1)
+            tags.split_off(pos + 1)
         } else {
             Vec::new()
         }
@@ -114,6 +172,7 @@ async fn get_tags(
 async fn get_manifest(
     extract::State(reg): extract::State<SafeReg>,
     extract::Path((name, reference)): extract::Path<(String, String)>,
+    range: Option<TypedHeader<Range>>,
 ) -> impl IntoResponse
 {
     let registry = reg.read().await;
@@ -123,18 +182,11 @@ async fn get_manifest(
         return NOT_FOUND.into_response();
     };
 
-    let body = body::Body::from_stream(ReaderStream::new(payload.file));
-    let headers = [
-        (http::header::CONTENT_TYPE, &payload.media_type),
-        (http::header::CONTENT_LENGTH, &format!("{}", payload.size)),
-        (HEADER_DIGEST, &payload.digest),
-    ];
-
     info!(
         "Manifest \"{}\" for \"{}\" found and served",
         reference, name
     );
-    (headers, body).into_response()
+    serve_file(payload, range).await
 }
 
 async fn head_manifest(
@@ -157,6 +209,7 @@ async fn head_manifest(
 async fn get_blob(
     extract::State(reg): extract::State<SafeReg>,
     extract::Path((name, digest)): extract::Path<(String, String)>,
+    range: Option<TypedHeader<Range>>,
 ) -> impl IntoResponse
 {
     let registry = reg.read().await;
@@ -166,15 +219,8 @@ async fn get_blob(
         return NOT_FOUND.into_response();
     };
 
-    let body = body::Body::from_stream(ReaderStream::new(payload.file));
-    let headers = [
-        (http::header::CONTENT_TYPE, &payload.media_type),
-        (http::header::CONTENT_LENGTH, &format!("{}", payload.size)),
-        (HEADER_DIGEST, &payload.digest),
-    ];
-
     info!("Blob \"{}\" for \"{}\" found and served", digest, name);
-    (headers, body).into_response()
+    serve_file(payload, range).await
 }
 
 async fn head_blob(
