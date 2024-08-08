9f93e8080a869e264c39aacfd38269667a81ca46
Zofia Abramowska <z.abramowska@samsung.com>
2024-08-08 17:30:50 +0200

    Client: Enhance errors

--- ir-client/src/error.rs
+++ ir-client/src/error.rs
@@ -1,4 +1,4 @@
-use std::io::Error as IOError;
+use std::{error::Error as StdError, fmt::Display, io::Error as IOError};
 
 #[derive(Debug)]
 pub enum Error {
@@ -17,6 +17,28 @@ pub enum Error {
 
     UnknownError,
 }
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::ConfigError(reqwest_error) => write!(f, "Configuration error: {}", reqwest_error)?,
+            Self::ConnectionError => f.write_str("Connection error")?,
+            Self::DigestInvalidError => f.write_str("Invalid digest format")?,
+            Self::IOError(io_error) => write!(f, "IO error: {}", io_error)?,
+            Self::JSONParsingError(json_error) => write!(f, "JSON parsing error: {}", json_error)?,
+            Self::ManifestFormatError => f.write_str("Invalid manifest format")?,
+            Self::ReferenceInvalidError => f.write_str("Invalid reference format")?,
+            Self::StatusError(status_error) => write!(f, "HTTP status error code: {}", status_error)?,
+            Self::TagInvalidError => f.write_str("Invalid tag format")?,
+            Self::UnknownError => f.write_str("Unknown error")?,
+            Self::UrlParsingError(url_error) => write!(f, "Url parsing error: {}", url_error)?,
+        }
+
+        Ok(())
+    }
+}
+
+impl StdError for Error {}
 
 impl From<url::ParseError> for Error {
     fn from(value: url::ParseError) -> Self {
